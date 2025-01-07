dc729faf00ec992f52d3611a06244b0f1f061452
is-itk <inimices@gmail.com>
2025-01-08 06:04:07 +0800

    Update vite.config.js

--- magang/frontend/vite.config.js
+++ magang/frontend/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// magang/frontend/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/akademik/magang/',
-})
+  base: '/akademik/magang/',  // Menentukan base path untuk subdirektori
+});
