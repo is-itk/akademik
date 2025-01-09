74949643cb78fd44f8684af98c6a8454962faf3c
is-itk <inimices@gmail.com>
2025-01-09 16:00:28 +0800

    Update hello.js

--- magang/backend/api/hello.js
+++ magang/backend/api/hello.js
@@ -1,6 +1,6 @@
 // magang/backend/api/hello.js
 
 export default function handler(req, res) {
-    res.status(200).json({ message: "Hello from Vercel!" });
+    return res.status(200).json({ message: "Hello from Vercel (ESM)!" });
   }
   
